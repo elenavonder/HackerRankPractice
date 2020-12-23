4d6e7aae6bde1e23e0a451f214700f3db49c23f0
elenavonder <evonder13@gmail.com>
2020-12-23 16:34:37 -0500

    added class

---
+++ Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HackerRankPractice
+{
+    public class Solution
+    {
+
+        public int a { get; set; }
+        public int b { get; set; }
+
+        public int solveMeFirst (int a, int b)
+        {
+            return a + b;
+        }
+
+        public Solution()
+        {
+        }
+    }
+}
